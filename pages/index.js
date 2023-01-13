c45b640a9b62e2afd82ee4d5ae5d0f0248279aeb
Matthew Hefferon <matthefferon@gmail.com>
2023-01-13 11:03:40 -0700

    Padding update

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 py-2">
+          <div className="text-center p-5 py-5">
             <div className="mx-auto bg-gradient-to-b rounded-full w-60 h-60 relative overflow-hidden md:h-70 md:w-70">
               <Image src={matthew} alt="Profile" layout="fill" objectFit="cover" />
             </div>
@@ -86,7 +86,7 @@ export default function Home() {
           </div>
         </section>
 
-        {/* <section className="py-10">
+        {/* <section className="py-5">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">
               Embedded Analytics @EVERFI
@@ -140,7 +140,7 @@ export default function Home() {
           </div>
         </section>*/}
 
-        <section className="py-10">
+        <section className="py-5">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">
               Embedded Analytics @Bunny
@@ -197,7 +197,7 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="py-10">
+        <section className="py-5">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">YouTube Channel</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
